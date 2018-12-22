494214029fa5974c6d9f8d0952c9237e5bf4f40b
xdd <jm_88rising@163.com>
2018-12-22 10:19:14 +0800

    first test

---
+++ Single.java
@@ -0,0 +1,43 @@
+package com.test;
+
+public class Single {
+    //饿汉式
+	/*private static Single single=new Single();
+	
+	private Single(){}
+	
+	public static Single getInstance(){
+		return single;
+	}
+	
+	*/
+	
+	
+	
+	//懒汉式
+	/*private static Single instance;
+	
+	private Single(){}
+	
+	public static   synchronized Single getInstance(){
+		if(instance==null){
+		   instance=new Single();
+		}
+		return instance;
+	}*/
+	
+	//双重锁机制，安全且多线程下安全效率高
+	private volatile static Single instance;
+	
+	private Single(){}
+	
+	public static Single getInstance(){
+		if(instance==null){
+			synchronized(Single.class){
+				//if()
+			}
+		}
+	}
+	
+	
+}
